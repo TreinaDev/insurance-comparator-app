e12585a07470f45e8cf40fc9cec1d217f80aa828
Anyelly Marrye Luvizott <any.luvizott@gmail.com>
2022-11-08 11:30:48 -0300

    Alterando mock no teste de busca

--- spec/system/search/visitor_search_insurance_spec.rb
+++ spec/system/search/visitor_search_insurance_spec.rb
@@ -22,14 +22,14 @@ describe 'Visitante realiza uma busca por seguradoras' do
 
     expect(current_path).to eq search_path
     expect(page).to have_content 'Resultado da Busca: iPhone 11'
-    expect(page).to have_content 'Seguradora: '
-    expect(page).to have_content 'Tipo de Pacote: '
-    expect(page).to have_content 'Valor da Contratação por 12 meses: R$ '
+    expect(page).to have_content 'Seguradora: Seguradora 1'
+    expect(page).to have_content 'Tipo de Pacote: Premium'
+    expect(page).to have_content 'Valor da Contratação por 12 meses: R$ 50,00'
   end
 
   it 'e não encontra seguradoras para o seu produto' do
-    fake_response = double('faraday_response', status: 200, body: '[]')
-    allow(Faraday).to receive(:get).with("http://localhost:4000/api/v1/insurance/#{@query}").and_return(fake_response)
+    dados_fake = []
+    allow(Insurance).to receive(:search).with('iPhone 11').and_return(dados_fake)
 
     visit root_path
     fill_in 'Produto',	with: 'iPhone 11'
