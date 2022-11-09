0d6c653bc00daf385b3e4818262cfdf183756bb6
Aline Moraes <alinemoraes@MacBook-Pro-de-Aline.local>
2022-11-09 15:41:53 -0300

    e não consegue visualizar o pacote

--- spec/system/search/visitor_views_insurance_details_spec.rb
+++ spec/system/search/visitor_views_insurance_details_spec.rb
@@ -21,4 +21,21 @@ describe 'visitante vê detalhes do pacote' do
     expect(page).to have_content 'Modelo do Produto: iPhone 11'
     expect(page).to have_content 'Valor de Contratação para 12 meses: R$ 50,00'
   end
+  it 'e não consegue visualizar o pacote' do
+    dados_fake = []
+    dados_fake << Insurance.new(id: 1, insurance_name: 'Seguradora 1', product_model: 'iPhone 11', packages: 'Premium',
+                                price: 50)
+    allow(Insurance).to receive(:search).with('iPhone 11').and_return(dados_fake)
+
+    json_data = Rails.root.join('spec/support/json/insurance.json').read
+    fake_response = double('faraday_response', success?: true, body: "{}")
+    allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/insurance/1').and_return(fake_response)
+ 
+    visit root_path
+    fill_in 'Produto',	with: 'iPhone 11'
+    click_on 'Buscar'
+    click_on 'Seguradora 1'
+    
+    expect(page).to have_content 'Não foi possível carregar as informações do pacote'
+  end
 end
