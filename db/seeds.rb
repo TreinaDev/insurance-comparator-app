bf91ee7e4553d749b0ba9fb44f9c4690680d675f
Karina Sakata <karina.msakata@hotmail.com>
2022-11-12 20:08:20 -0300

    Complementando teste de validação de cpf

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ user_ana = Client.create!(name: 'Ana Lima', email: 'ana@gmail.com', password: '1
                           address: 'Rua Dr Nogueira Martins, 680', city: 'São Paulo', state: 'SP',
                           birth_date: '29/10/1997')
 
-Equipment.new(client: user_ana, name: 'Iphone 14 - ProMax', brand: 'Apple', purchase_date: Time.zone.today,
-              invoice: fixture_file_upload('spec/support/invoice.png'),
-              photos: [fixture_file_upload('spec/support/photo_1.png'),
-                       fixture_file_upload('spec/support/photo_2.jpg')])
+# Equipment.new(client: user_ana, name: 'Iphone 14 - ProMax', brand: 'Apple', purchase_date: Time.zone.today,
+#               invoice: fixture_file_upload('spec/support/invoice.png'),
+#               photos: [fixture_file_upload('spec/support/photo_1.png'),
+#                        fixture_file_upload('spec/support/photo_2.jpg')])
--- spec/system/cpf_verification/client_has_his_cpf_verified_spec.rb
+++ spec/system/cpf_verification/client_has_his_cpf_verified_spec.rb
@@ -8,13 +8,31 @@ describe 'Usuário tem seu CPF consultado na aplicação Anti-Fraude' do
     ana = Client.create!(name: 'Ana Lima', email: 'ana@gmail.com', password: '12345678', cpf: '21234567890',
                          address: 'Rua Dr Nogueira Martins, 680', city: 'São Paulo', state: 'SP',
                          birth_date: '29/10/1997')
-    #preciso de um dispositivo
-    #preciso de uma compra
+                         
+    iphone = Equipment.create!(client: ana, name: 'Iphone 14 - ProMax', brand: 'Apple', purchase_date: Time.zone.today,
+                               invoice: fixture_file_upload('spec/support/invoice.png'),
+                               photos: [fixture_file_upload('spec/support/photo_1.png'),
+                               fixture_file_upload('spec/support/photo_2.jpg')])
 
-    login_as(ana)
+    insurances = []
+    insurances << Insurance.new(id: 1, insurance_name: 'Seguradora 1', product_model: 'iPhone 11', packages: 'Premium',
+                                price: 50)
+
+    insurances << Insurance.new(id: 2, insurance_name: 'Seguradora 2', product_model: 'iPhone 11', packages: 'Plus',
+                                price: 20)
+
+    allow(Insurance).to receive(:search).with('iPhone 11').and_return(insurances)
+
+    insurance = Insurance.new(id: 1, insurance_name: 'Seguradora 1', product_model: 'iPhone 11', packages: 'Premium',
+                              price: 50)
+
+    allow(Insurance).to receive(:find).with('1').and_return(insurance)
+
     visit root_path
-    click_on 'Meus dispositivos'
-    #qual caminho até a tela de compra?
+    fill_in 'Produto', with: 'iPhone 11'
+    click_on 'Buscar'
+    click_on 'Seguradora 1'
+    click_on 'Contratar Seguro'
 
     expect(page).to have_content 'Status da Compra: CPF bloqueado'
   end
