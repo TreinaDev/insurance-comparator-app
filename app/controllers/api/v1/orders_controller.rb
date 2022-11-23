96274a574631b291e08d589533480aa59d30df19
Aline Moraes <alinemoraes@MacBook-Pro-de-Aline.local>
2022-11-23 17:11:24 -0300

    merge com a main

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::OrdersController < Api::V1::ApiController
 		equipment = Equipment.find(order.equipment_id)
 		client = Client.find(order.client_id)
 		order_api = OrderApi.new(order, equipment, client)
-    render status: :ok, json: order_api.as_json
+    	render status: :ok, json: order_api.as_json
   end
 end
 
