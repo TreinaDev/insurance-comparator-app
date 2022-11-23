3be0c7ddff6243c07583faa9f5e998fc979ace8d
Anyelly Marrye Luvizott <any.luvizott@gmail.com>
2022-11-22 21:29:13 -0300

    Acrescenta testes

--- spec/system/voucher_validation_spec.rb
+++ spec/system/voucher_validation_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 describe 'Cliente insere um cupom de desconto' do
   it 'e o cupom é valido' do
+    ana = Client.create!(name: 'Ana Lima', email: 'ana@gmail.com', password: '12345678', cpf: '21234567890',
+                         address: 'Rua Dr Nogueira Martins, 680', city: 'São Paulo', state: 'SP',
+                         birth_date: '29/10/1997')
+
+    order = Order.create!(client: ana, equipment:, payment_method:, contract_period: 10, insurance_id: 45,
+                          price_percentage: 5, insurance_name: 'Seguradora 45', packages: 'Premium',
+                          insurance_model: 'iPhone 11', status:, voucher_name: nil, voucher: nil)
+
+    order_result = Order.create!(client: ana, equipment:, payment_method:, contract_period: 10, insurance_id: 45,
+                                 price_percentage: 5, insurance_name: 'Seguradora 45', packages: 'Premium',
+                                 insurance_model: 'iPhone 11', status:, voucher_name: 'ABC123', voucher: 20)
+
+    allow(payment).to receive(:voucher_validation).and_return(order_result)
+
+    login_as(ana)
     visit root_path
     click_on 'Meus Pedidos'
     click_on 'iPhone 11'
@@ -15,6 +30,15 @@ describe 'Cliente insere um cupom de desconto' do
   end
 
   it 'e o cupom é inválido' do
+    ana = Client.create!(name: 'Ana Lima', email: 'ana@gmail.com', password: '12345678', cpf: '21234567890',
+                         address: 'Rua Dr Nogueira Martins, 680', city: 'São Paulo', state: 'SP', birth_date: '29/10/1997')
+
+    order = Order.create!(client: ana, equipment:, payment_method:, contract_period: 10, insurance_id: 45,
+                          price_percentage: 5, insurance_name: 'Seguradora 45', packages: 'Premium', insurance_model: 'iPhone 11', status:, voucher_name: nil, voucher: nil)
+
+    allow(payment).to receive(:voucher_validation).and_return(order)
+
+    login_as(ana)
     visit root_path
     click_on 'Meus Pedidos'
     click_on 'iPhone 11'
@@ -28,6 +52,16 @@ describe 'Cliente insere um cupom de desconto' do
   end
 
   it 'e o cupom está expirado' do
+    ana = Client.create!(name: 'Ana Lima', email: 'ana@gmail.com', password: '12345678', cpf: '21234567890',
+                         address: 'Rua Dr Nogueira Martins, 680', city: 'São Paulo', state: 'SP', birth_date: '29/10/1997')
+
+    order = Order.create!(client: ana, equipment:, payment_method:, contract_period: 10, insurance_id: 45,
+                          price_percentage: 5, insurance_name: 'Seguradora 45', packages: 'Premium',
+                          insurance_model: 'iPhone 11', status:, voucher_name: nil, voucher: nil)
+
+    allow(payment).to receive(:voucher_validation).and_return(order)
+
+    login_as(ana)
     visit root_path
     click_on 'Meus Pedidos'
     click_on 'iPhone 11'
