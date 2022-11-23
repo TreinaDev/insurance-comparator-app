7b9c8d6087dca343481b15679196b4c4bbbb3da9
Aline Moraes <alinemoraes@MacBook-Pro-de-Aline.local>
2022-11-23 15:13:41 -0300

    adicionado campo voucher_code

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_182852) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_180013) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_182852) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -84,12 +84,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_182852) do
     t.string "package_name"
     t.string "product_model"
     t.decimal "final_price"
-    t.decimal "voucher"
+    t.decimal "voucher_price"
     t.string "code"
     t.integer "max_period"
     t.integer "min_period"
     t.string "product_category"
     t.string "product_category_id"
+    t.string "voucher_code"
     t.index ["client_id"], name: "index_orders_on_client_id"
     t.index ["equipment_id"], name: "index_orders_on_equipment_id"
   end
