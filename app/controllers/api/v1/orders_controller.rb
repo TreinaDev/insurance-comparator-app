9a5cb29aa670adcca26e9c88650c6fa4eec3edbb
Aline Moraes <alinemoraes@MacBook-Pro-de-Aline.local>
2022-11-24 13:35:07 -0300

    corrigindo ofensa apontada pelo rubocop

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::OrdersController < Api::V1::ApiController
     Equipment.find(order.equipment_id)
     Client.find(order.client_id)
     render status: :ok, json: order.as_json(include: { equipment: { except: %i[created_at updated_at client_id id] },
-                                            client: { except: %i[created_at updated_at id] } },
+                                                       client: { except: %i[created_at updated_at id] } },
                                             except: %i[created_at updated_at equipment_id client_id])
   end
 end
