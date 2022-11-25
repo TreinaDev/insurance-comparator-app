cde7909711f621e3337d6f739124065f4f2acd95
Aline Moraes <alinemoraes@MacBook-Pro-de-Aline.local>
2022-11-25 18:38:11 -0300

    post de apólice incluído
    Co-Authored_by:Mariana Souza <marianaletii@gmail.com>

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -26,10 +26,9 @@ RSpec.describe Order, type: :model do
         .with("#{Rails.configuration.external_apis['payment_fraud_api']}/blocked_registration_numbers/#{cpf}")
         .and_return(fake_response)
 
-      order.validate_cpf(order.client.cpf)
-      result = order.status
+      result = order.validate_cpf(order.client.cpf)
 
-      expect(result).to eq 'insurance_company_approval'
+      expect(result).to be true
     end
 
     it 'e retorna CPF bloqueado' do
@@ -59,8 +58,66 @@ RSpec.describe Order, type: :model do
       result = order.status
 
       expect(result).to eq 'cpf_disapproved'
+      expect(order.validate_cpf(order.client.cpf)).to be false
     end
   end
+
+  context '#post_policy' do
+    it 'e retorna apólice pendente criada com sucesso' do
+      client = Client.create!(name: 'Ana Lima', email: 'ana@gmail.com', password: '12345678', cpf: '21234567890',
+                            address: 'Rua Dr Nogueira Martins, 680', city: 'São Paulo', state: 'SP',
+                            birth_date: '29/10/1997')
+      equipment = Equipment.create!(client:, name: 'iphone 11', brand: 'Apple', equipment_price: 1000, purchase_date: '01/11/2022',
+                      invoice: fixture_file_upload('spec/support/invoice.png'),
+                      photos: [fixture_file_upload('spec/support/photo_1.png'),
+                               fixture_file_upload('spec/support/photo_2.jpg')])
+      order = Order.create!(client:, equipment:, contract_period: 10, insurance_company_id: 45,
+                            price: 10.00, final_price: 100, insurance_name: 'Seguradora 45',
+                            package_name: 'Premium', product_category_id: 2, product_category: 'iPhone 11',
+                            status: :pending, package_id: 2)
+      json_data = Rails.root.join('spec/support/json/policy.json').read
+      fake_response = double('faraday_response', success?: true, body: json_data)
+      params =  { policy: { client_name: client.name, client_registration_number: client.cpf, 
+      client_email: client.email, policy_period: order.contract_period, order_id: order.id,
+      package_id: order.package_id, insurance_company_id: order.insurance_company_id, equipment_id: order.equipment_id} }
+      allow(Faraday).to receive(:post)
+                              .with("#{Rails.configuration.external_apis['insurance_api']}/policies/", params )
+                              .and_return(fake_response)
+                      
+      result = order.post_policy
+
+      expect(order.status).to eq('insurance_company_approval')
+      expect(result).to be true
+    end
+
+    it 'e aplicação está fora do ar' do
+      client = Client.create!(name: 'Ana Lima', email: 'ana@gmail.com', password: '12345678', cpf: '21234567890',
+                            address: 'Rua Dr Nogueira Martins, 680', city: 'São Paulo', state: 'SP',
+                            birth_date: '29/10/1997')
+      equipment = Equipment.create!(client:, name: 'iphone 11', brand: 'Apple', equipment_price: 1000, purchase_date: '01/11/2022',
+                      invoice: fixture_file_upload('spec/support/invoice.png'),
+                      photos: [fixture_file_upload('spec/support/photo_1.png'),
+                               fixture_file_upload('spec/support/photo_2.jpg')])
+      order = Order.new(client:, equipment:, contract_period: 10, insurance_company_id: 45,
+                            price: 10.00, final_price: 100, insurance_name: 'Seguradora 45',
+                            package_name: 'Premium', product_category_id: 2, product_category: 'iPhone 11',
+                            status: :pending, package_id: 2)
+
+      fake_response = double('faraday_response', success?: false, status: 500)
+      params =  { policy: { client_name: client.name, client_registration_number: client.cpf, 
+      client_email: client.email, policy_period: order.contract_period, order_id: order.id,
+      package_id: order.package_id, insurance_company_id: order.insurance_company_id, equipment_id: order.equipment_id} }
+      allow(Faraday).to receive(:post)
+                              .with("#{Rails.configuration.external_apis['insurance_api']}/policies/", params )
+                              .and_return(fake_response)
+
+      result = order.post_policy
+
+      expect(order.status).to eq('pending')
+      expect(result).to be false
+    end
+  end
+
 
   describe '#total_price' do
     it 'falso quando preço diferente do esperado' do
