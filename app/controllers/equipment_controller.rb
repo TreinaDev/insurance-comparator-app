4c4ed1730be6c0f22ae4f58b74a696b1c90cff46
Thalis Freitas <ofthalis@gmail.com>
2022-11-25 20:47:14 -0300

    adiciona testes de request para equipment

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,7 +1,7 @@
 class EquipmentController < ApplicationController
   before_action :authenticate_client!
   before_action :set_equipment, only: %i[show edit update edit]
-  before_action :cannot_belong_to_an_order, only: %i[edit]
+  before_action :cannot_belong_to_an_order, only: %i[edit update]
 
   def index
     @equipment = current_client.equipment
--- spec/system/equipments/user_edit_equipment_spec.rb
+++ spec/system/equipments/user_edit_equipment_spec.rb
@@ -78,20 +78,20 @@ describe 'Usuário edita um dispositivo' do
     expect(current_path).to eq equipment_path(equipment.id)
   end
 
-  it 'se o dispositivo não estiver vinculado a um pedido' do 
+  it 'se o dispositivo não estiver vinculado a um pedido' do
     client = Client.create!(name: 'Usuário 1', cpf: '60536252050', address: 'Rua Primavera, 424', city: 'Bauru',
-                          state: 'SP', birth_date: '12/05/1998', email: 'usuario@email.com', password: 'password')
-    equipment = Equipment.create!(client: client, name: 'Iphone 14 - ProMax', brand: 'Apple', purchase_date: '01/11/2022',
+                            state: 'SP', birth_date: '12/05/1998', email: 'usuario@email.com', password: 'password')
+    equipment = Equipment.create!(client:, name: 'Iphone 14 - ProMax', brand: 'Apple', purchase_date: '01/11/2022',
                                   invoice: fixture_file_upload('spec/support/invoice.png'), equipment_price: 10_199,
                                   photos: [fixture_file_upload('spec/support/photo_1.png'),
-                                          fixture_file_upload('spec/support/photo_2.jpg')])
+                                           fixture_file_upload('spec/support/photo_2.jpg')])
     insurance = Insurance.new(id: 67, name: 'Super Econômico', max_period: 18, min_period: 6, insurance_company_id: 45,
                               insurance_name: 'Seguradora 45', price: 100.00, product_category_id: 1,
                               product_category: 'Telefone', product_model: 'iPhone 11')
-    order = Order.create!(status: :insurance_approved, contract_period: 9, equipment:,
-                          client:, insurance_name: insurance.insurance_name, package_name: insurance.name,
-                          product_model: insurance.product_category, price: insurance.price,
-                          insurance_company_id: insurance.insurance_company_id)
+    Order.create!(status: :insurance_approved, contract_period: 9, equipment:,
+                  client:, insurance_name: insurance.insurance_name, package_name: insurance.name,
+                  product_model: insurance.product_category, price: insurance.price,
+                  insurance_company_id: insurance.insurance_company_id)
 
     login_as client
     visit edit_equipment_path(equipment)
