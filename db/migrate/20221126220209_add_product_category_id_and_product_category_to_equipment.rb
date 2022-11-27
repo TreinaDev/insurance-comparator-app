de5fd186ce9021083069ac5e2359b25dc0faf2dd
Sade Oliveira <sade.oli95@gmail.com>
2022-11-27 18:28:00 -0300

    Ajustes Gerais

--- db/migrate/20221126220209_add_product_category_id_and_product_category_to_equipment.rb
+++
@@ -1,6 +0,0 @@
-class AddProductCategoryIdAndProductCategoryToEquipment < ActiveRecord::Migration[7.0]
-  def change
-    add_column :equipment, :product_category_id, :integer
-    add_column :equipment, :product_category, :string
-  end
-end
